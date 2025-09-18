24e94875a8ee75bddf7f9c34ea786270245b9c1c
Nandish Rao <nandishpvtmail@gmail.com>
2025-09-18 19:06:12 +0530

    updated the git ignore

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -1,12 +1,14 @@
 import { MovieCardIMG_CDN_URL } from "../Utils/Constants";
 
-const MovieCard=({posterPath})=>{
+const MovieCard=( {posterPath })=>{
    return(
      <div className="w-36 sm:w-40 md:w-44 lg:w-48 flex-shrink-0">
         <img 
          className="w-full h-auto rounded-lg hover:scale-105 transition-transform duration-200" 
         alt="Movie-poster"
         src={MovieCardIMG_CDN_URL+posterPath}/>
+       
+       
     </div>
    )
 }
